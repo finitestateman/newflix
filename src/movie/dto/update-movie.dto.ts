fb056e23e44a11d6ad4983e5188e115db95c8c32
Finite Stateman <finitestateman@gmail.com>
2026-02-14 22:01:04 +0900

    01. 기본 Validator 정리

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,11 +1,33 @@
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
+
+enum MovieGenre {
+    Aaction = 'action',
+    Fantasy = 'fantasy',
+}
 
 export class UpdateMovieDto {
-    @IsNotEmpty()
-    @IsOptional()
+    @IsNotEmpty() // 값이 있을 때 빈 문자열이면 안 된다
+    @IsOptional() // 값이 선택적
     title?: string;
 
     @IsNotEmpty()
     @IsOptional()
     genre?: string;
+
+    // @IsDefined() // null || undefined (비허용)
+    // @IsOptional()
+    // @Equals('code factory')
+    // @NotEquals('code factory')
+    // @IsEmpty() // null || undefined || '' 만 허용(' ' 비허용)
+    // @IsNotEmpty() // IsDefined || ''
+    // @IsIn(['action', 'fantasy'])
+    // @IsNotIn(['romance'])
+    // @IsBoolean()
+    // @IsNumber()
+    // @IsInt()
+    // @IsArray()
+    // @IsEnum(MovieGenre)
+    // @IsDate() // 실제 날짜 객체여야 함
+    @IsDateString() // 날짜 형식의 문자열
+    test?: string;
 }
