55a6636c0736b0ff3c36c214dbaeca0053eab377
Finite Stateman <finitestateman@gmail.com>
2026-02-16 19:37:37 +0900

    04. TypeORM으로 테이블 생성하기

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,5 +1,29 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+    VersionColumn,
+} from 'typeorm';
+
+@Entity()
 export class Movie {
+    @PrimaryGeneratedColumn()
     declare id: number;
+
+    @Column()
     declare title: string;
+
+    @Column()
     declare genre: string;
+
+    @CreateDateColumn()
+    declare createdAt: Date;
+
+    @UpdateDateColumn()
+    declare updatedAt: Date;
+
+    @VersionColumn()
+    declare version: number;
 }
